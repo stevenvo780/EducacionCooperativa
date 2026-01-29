0cc334ca0688089b126424f02691a09d558f49d6
Steven Vallejo Ortiz <stevenvallejo780@gmail.com>
2026-01-29 16:58:46 -0500

    fix: use Terser with keep_classnames to fix Super constructor null

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,29 @@
+import TerserPlugin from 'terser-webpack-plugin';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  // Disable SWC minification to fix "Super constructor null" with xterm/mosaic classes
   swcMinify: false,
-  // Allow imports from outside if needed (monorepo), but here we are self-contained
   transpilePackages: ['react-mosaic-component', 'firebase', 'undici', '@xterm/xterm', '@xterm/addon-fit', '@xterm/addon-web-links'],
   experimental: {
     serverComponentsExternalPackages: ['firebase-admin'] 
-  }
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.optimization.minimizer = config.optimization.minimizer?.filter(
+        (m) => m.constructor.name !== 'TerserPlugin'
+      ) || [];
+      config.optimization.minimizer.push(
+        new TerserPlugin({
+          terserOptions: {
+            keep_classnames: true,
+            keep_fnames: true,
+          },
+        })
+      );
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
