4cdb12cb489c7f0221fd3104eaa20a078d1bd717
stev <stevenvallejo780@gmail.com>
2026-04-12 13:15:20 +0000

    fix: use const for editable (eslint prefer-const)

--- src/generated/st-runtime-manifest.ts
+++ src/generated/st-runtime-manifest.ts
@@ -5,7 +5,7 @@
  */
 
 export const ST_RUNTIME_MANIFEST = {
-  "generatedAt": "2026-04-12T05:13:03.533Z",
+  "generatedAt": "2026-04-12T13:14:10.321Z",
   "canonicalVersion": "3.0.2",
   "packageVersion": "3.0.2",
   "cliVersion": "2.6.0",
